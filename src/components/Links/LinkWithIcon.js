ea01fe7e48ae2da186075a4feb4a28f10b8eba2c
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-06-22 12:31:54 +0530

    feat: add icons

--- src/components/Links/LinkWithIcon.js
+++ src/components/Links/LinkWithIcon.js
@@ -1,10 +1,10 @@
-const LinkWithIcon = ({linkText}) => {
-    return ( 
+const LinkWithIcon = ({ linkText }) => {
+    return (
         <a href="/login" className="link">
             <span className="link-icon"></span>
             <span>{linkText}</span>
         </a>
-     );
+    );
 }
- 
+
 export default LinkWithIcon;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,7 @@ import styles from './search.scss';
 const Search = () => {
     return (
         <div className="search">
-            <span role="search" aria-label="my search icon" className="icon icon-search"></span>
+            <span role="search" aria-label="my search icon"><img src={`${process.env.PUBLIC_URL}/images/icons/search.svg`} />Search</span>
             <Input type="text" classes="form-control" placeholder="What would you like to find?" />
             <button type="button">close</button>
         </div>
