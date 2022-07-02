2fd0120006791053cd660a08862fea4f5e6a8c21
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-07-03 02:46:07 +0530

    feat: add accordion

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,12 +1,36 @@
 
 import { useSelector } from "react-redux";
 import CartList from '../components/Cart/CartList';
+import AccordionList from "../components/Accordion/AccordionList/AccordionList";
+
+const accordion = [
+    {
+        id: "accordion__101",
+        key: "Estimate your Shipping",
+        value: "Shipping to 91001"
+    },
+
+    {
+        id: "accordion__102",
+        key: "Enter a Coupon Code",
+        value: "20% discount applied"
+    },
+
+    {
+        id: "accordion__103",
+        key: "Apply Gift Card",
+        value: " "
+    }
+]
 
 const CartPage = () => {
     const cartItems = useSelector(state => state.cartItems);
 
     return (
-        <CartList cartItems={cartItems} />
+        <>
+            <CartList cartItems={cartItems} />
+            <AccordionList accordionItems={accordion} />
+        </>
     );
 }
 
