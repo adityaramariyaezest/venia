c3a0058240ba5a0d179f1f845d70d7c78558d54f
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-07-02 18:30:12 +0530

    feat: add cart system sending id from link component

--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -1,3 +1,4 @@
+import { useDispatch } from "react-redux";
 import Button from '../Button/Button';
 import Quantity from '../Quantity/Quantity';
 import ProducutColors from './ProductColors';
@@ -6,9 +7,20 @@ import Breadcrumb from '../Breadcrumb/Breadcrumb';
 import Share from '../Share/Share';
 import FlexBox from '../Layout/Flexbox';
 import LinkWithIcon from "../Links/LinkWithIcon";
+import { addToCart } from '../../actions/cartActions'
+import { useLocation } from 'react-router-dom'
 import product from './product.module.scss';
 
 const ProductDetails = () => {
+    const dispatch = useDispatch();
+    const location = useLocation()
+    const { productID } = location.state
+
+    const handleEvent = (e, productId) => {
+        console.log("I was clicked" + productId);
+        dispatch(addToCart(productId));
+    };
+
     return (
         <>
             <Breadcrumb />
@@ -19,7 +31,8 @@ const ProductDetails = () => {
                 <ProducutColors />
                 <ProducutSize />
                 <Quantity isLabel />
-                <Button classes="block" variant="primary" text="add to cart" />
+
+                <Button classes="block" variant="primary" text="add to cart" onPress={e => handleEvent(e, productID)} />
                 <Share classes={product.product__share}>
                     <FlexBox classes="d-flex">
                         <LinkWithIcon linkText="Save" linkIcon="heart.svg" />
--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,11 +1,16 @@
 import Slider from "../components/Slider/Slider";
 import ProductDetails from "../components/Product/ProductDetails";
+import Wrapper from '../components/Wrapper/Wrapper';
 
 const ProductDetailsPage = () => {
     return (
-        <div className="aem-Grid aem-Grid--12">
-            <Slider />
-            <ProductDetails />
+        <div className="aem-Grid aem-Grid--10">
+            <Wrapper phone="10" tablet="5" desktop="5">
+                <Slider />
+            </Wrapper>
+            <Wrapper phone="10" tablet="3" desktop="3">
+                <ProductDetails />
+            </Wrapper>
         </div>
     );
 }
