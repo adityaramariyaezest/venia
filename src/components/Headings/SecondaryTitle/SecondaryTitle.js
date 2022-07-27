62d5b6a73317b15ce05824153115171bbe323fa8
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-07-27 13:55:38 +0530

    feat: add titles to checkout page

---
+++ src/components/Headings/SecondaryTitle/SecondaryTitle.js
@@ -0,0 +1,6 @@
+
+import sc from './secondaryTitle.module.scss';
+
+const SecondaryTitle = ({ title }) => <h2 className={sc.title}>{title}</h2>
+
+export default SecondaryTitle;
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -2,6 +2,9 @@ import Container from "../components/Container/Container";
 import Bill from "../components/Cart/Bill/Bill";
 import Wrapper from "../components/Wrapper/Wrapper";
 import Title from "../components/Headings/Title/Title";
+import SecondaryTitle from '../components/Headings/SecondaryTitle/SecondaryTitle';
+import Lead from "../components/Lead/Lead";
+import Description from "../components/Description/Description";
 
 const CheckoutPage = () => {
     return (
@@ -9,7 +12,9 @@ const CheckoutPage = () => {
             <div className="aem-Grid aem-Grid--12">
                 <Title title="Checkout" isSpecial />
                 <Wrapper phone="12" tablet="12" desktop="8">
-                    <p>guest</p>
+                    <SecondaryTitle title="guest checkout" />
+                    <Lead text="Contact information" />
+                    <Description content="We’ll use these details to keep you informed on your delivery." />
                 </Wrapper>
 
                 <Wrapper phone="12" tablet="12" desktop="4">
