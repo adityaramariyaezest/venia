3386fab6cd0a851ae40ec0c3a1de502ea2c424bc
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-07-30 11:27:57 +0530

    feat: add custom checkboxes

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,28 @@
 import cn from 'classnames';
+
+import { SquareIcon, CheckIcon } from '../Icons/Icons';
+
 import input from './input.module.scss';
+
+
+const CustomCheckBox = () => {
+    return (
+        <div className={input['custom-checkbox']}>
+            <SquareIcon />
+            <CheckIcon />
+        </div>
+    )
+}
 
 const Input = ({ id, labelText, classes, type, placeholder, name, value, direction, custom, onChange }) => {
     return (
         <div className={cn(input['form-group'], custom ? { [input[`form-group--${custom}`]]: custom } : '', type ? { [input[`form-group--${type}`]]: type } : '', direction ? { [input[`form-group--${direction}`]]: direction } : '')}>
             <label className={input.label} for={id}>{labelText}</label>
             <input id={id} type={type} className={classes} name={name} placeholder={placeholder} required value={value ? value : null} onChange={onChange} />
+
+            {type === 'checkbox' ? <CustomCheckBox />
+                : null
+            }
         </div >
     )
 }
