b2fb2eaac9e8d58eaa3d7b401b5b6a3934ee47d1
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-08-04 14:20:14 +0530

    feat: add category images in homepage

--- src/components/Product/ProductCategories/ProductCategory.js
+++ src/components/Product/ProductCategories/ProductCategory.js
@@ -2,12 +2,14 @@ import product from "./productCategory.module.scss";
 import Wrapper from "../../Wrapper/Wrapper";
 import { Link } from "react-router-dom";
 const ProductCategory = ({ Id, title }) => {
+    console.log('title', title)
+
     return (
         <Wrapper phone="12" tablet="6" desktop="3">
             <div className={product.product}>
                 <Link to="/products" state={{ productID: Id }}>
                     <div className={product.product__image}>
-                        <img src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg' alt={title} height="150" />
+                        <img src={`${process.env.PUBLIC_URL}/${title}.jpg`} alt={title} height="150" />
                     </div>
                     <div className={product.product__details}>
                         <p className={product.product__name}>{title}</p>
