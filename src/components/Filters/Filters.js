f7692e69bc8ac0276866bbed20903860b9e6b1c8
adityaramariyaezest <aditya.ramariya22@gmail.com>
2022-08-04 13:44:31 +0530

    feat: add filters on mobile

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 import Checkbox from "../Checkbox/Checkbox";
+import Button from '../Button/Button';
+import { LockIcon } from '../Icons/Icons';
 
 import { setToLocalStorage } from '../../common/common';
 import './filters.scss';
+import FlexBox from '../Layout/Flexbox';
 
-const Filters = ({ filterByTitle, onFilter, checkedProducts }) => {
+const Filters = ({ filterByTitle, onFilter, checkedProducts, showFilters, hideFilters, totalProducts }) => {
     const [categories, setCategories] = useState([]);
     setToLocalStorage('categories', categories);
 
@@ -18,14 +21,40 @@ const Filters = ({ filterByTitle, onFilter, checkedProducts }) => {
     }, [])
 
     return (
-        <article>
-            <p>{filterByTitle}</p>
-            {categories && categories.map((category, index) => <Checkbox
-                key={index} id={`category__${index}`}
-                filterCheckbox={category}
-                direction="row-reverse"
-                onFilter={onFilter} />)}
-        </article>
+        <div className={showFilters ? 'filters-wrapper filters-wrapper--active' : 'filters-wrapper'}>
+            <FlexBox classes="d-flex__justify__content__between d-flex--align-center sidebar-title">
+                <p>Filters</p>
+                <Button text=""
+                    type="link"
+                    variant="default"
+                    icon={LockIcon}
+                    isIcon
+                    svgIconMode="dark"
+                    imageDescription="checkout-button-icon"
+                    onPress={hideFilters} />
+            </FlexBox>
+
+            <article>
+                <p>{filterByTitle}</p>
+                {categories && categories.map((category, index) => <Checkbox
+                    key={index} id={`category__${index}`}
+                    filterCheckbox={category}
+                    direction="row-reverse"
+                    onFilter={onFilter} />)}
+            </article>
+
+            <article className='d-flex d-flex--align-center d-flex__justify-center visible-mobile'>
+                <Button
+                    text={`see ${totalProducts} Results`}
+                    type="solid"
+                    variant="primary"
+                    svgIconMode="dark"
+                    imageDescription="checkout-button-icon"
+                    onPress={hideFilters}
+                />
+            </article>
+        </div>
+
     );
 }
 
--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -7,7 +7,9 @@ import ProductList from "../components/Product/ProductList";
 import Sorting from "../components/Sorting/Sorting";
 import Banner from "../components/Banner/Banner";
 import Container from '../components/Container/Container';
-
+import { SlidersIcon } from '../components/Icons/Icons';
+import Button from '../components/Button/Button';
+import FlexBox from '../components/Layout/Flexbox';
 
 const sortOptions = [
     {
@@ -25,6 +27,9 @@ const Home = () => {
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
     const [checkedProducts, setCheckedProducts] = useState([]);
+
+    const [showFilters, setShowFilters] = useState(false);
+
 
     const getFilteredProducts = () => products.filter(product => checkedProducts.includes(product.category));
 
@@ -48,6 +53,10 @@ const Home = () => {
         );
     }
 
+    const handleClick = () => setShowFilters(!showFilters);
+
+    const hideFilters = () => setShowFilters(!showFilters);
+
     return (
         <>
             <div className="aem-Grid aem-Grid--12">
@@ -60,8 +69,18 @@ const Home = () => {
                     <Wrapper phone="12" tablet="3" desktop="3">
                         <Sidebar>
                             <Breadcrumb />
-                            <p className='sidebar-title'>Filters</p>
-                            <Filters filterByTitle="categories" onFilter={handleCategoryFilter} checkedProducts={checkedProducts} />
+                            <FlexBox classes="d-flex__justify-center d-flex--align-center visible-mobile">
+                                <Button size="small" text="Filter Results" type="link" variant="default" icon={SlidersIcon} isIcon onPress={handleClick} />
+                                <Button size="small" text="Sort Products" type="link" variant="default" icon={SlidersIcon} isIcon />
+                            </FlexBox>
+                            <Filters
+                                hideFilters={hideFilters}
+                                filterByTitle="categories"
+                                onFilter={handleCategoryFilter}
+                                checkedProducts={checkedProducts}
+                                showFilters={showFilters}
+                                totalProducts={filteredProducts.length > 0 ? filteredProducts.length : products.length}
+                            />
                         </Sidebar>
                     </Wrapper>
                     <Wrapper phone="12" tablet="9" desktop="9">
